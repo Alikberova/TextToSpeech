a80645c8ef91d6582379bda7e76dcf2697b0531b
yevheniia.alikberova <yevheniia.alikberova@warrcloud.com>
2023-12-26 02:32:35 +0100

    fix

--- BookToAudio/Extensions/ServicesDiExtension.cs
+++ BookToAudio/Extensions/ServicesDiExtension.cs
@@ -11,15 +11,10 @@ public static class ServicesDiExtension
 {
     public static IServiceCollection AddServices(this IServiceCollection services)
     {
-        //local
         services.AddScoped<AuthenticationService>();
-
-        //core
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<UserService>();
         services.AddScoped<BtaUserManager>();
-
-        //core
         services.AddScoped<ITokenService, TokenService>();
 
         return services;
