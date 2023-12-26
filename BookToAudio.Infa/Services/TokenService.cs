ecd63af9bce4823e31305bc714dac885fd704201
yevheniia.alikberova <yevheniia.alikberova@warrcloud.com>
2023-12-26 02:21:31 +0100

    refactoring

--- BookToAudio.Infa/Services/TokenService.cs
+++ BookToAudio.Infa/Services/TokenService.cs
@@ -1,46 +1,28 @@
 ﻿using BookToAudio.Core.Config;
 using BookToAudio.Core.Entities;
-using Microsoft.AspNetCore.Identity;
+using BookToAudio.Core.Services.Interfaces;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace BookToAudio.Core.Services;
+namespace BookToAudio.Infa.Services;
 
-public class AuthenticationService
+public class TokenService : ITokenService
 {
-    private readonly UserManager<User> _userManager;
-    private readonly BtaUserManager _btaUserManager;
     private readonly Jwt _jwtConfig;
 
-    public AuthenticationService(UserManager<User> userManager,
-        BtaUserManager btaUserManager,
-        IOptions<Jwt> jwtOptions)
+    public TokenService(IOptions<Jwt> jwtConfigOptions)
     {
-        _userManager = userManager;
-        _btaUserManager = btaUserManager;
-        _jwtConfig = jwtOptions.Value;
+        _jwtConfig = jwtConfigOptions.Value;
     }
 
-    public async Task<string> Login(string userName, string password)
-    {
-        var user = await _btaUserManager.FindByNameAsync(userName);
-
-        if (user is not null && await _userManager.CheckPasswordAsync(user, password))
-        {
-            return GenerateToken(user);
-        }
-
-        return string.Empty;
-    }
-
-    private string GenerateToken(User user)
+    public string GenerateToken(User user)
     {
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfig.Symmetric.Key));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
+        //todo token expiration and refresh
         var claims = new List<Claim>
         {
             new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
---
+++ BookToAudio/Services/AuthenticationService.cs
@@ -0,0 +1,34 @@
+﻿using BookToAudio.Core.Entities;
+using BookToAudio.Core.Services;
+using BookToAudio.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookToAudio.Services;
+
+public class AuthenticationService
+{
+    private readonly UserManager<User> _userManager;
+    private readonly BtaUserManager _btaUserManager;
+    private readonly ITokenService _tokenService;
+
+    public AuthenticationService(UserManager<User> userManager,
+        BtaUserManager btaUserManager,
+        ITokenService tokenService)
+    {
+        _userManager = userManager;
+        _btaUserManager = btaUserManager;
+        _tokenService = tokenService;
+    }
+
+    internal async Task<string> Login(string userName, string password)
+    {
+        var user = await _btaUserManager.FindByNameAsync(userName);
+
+        if (user is not null && await _userManager.CheckPasswordAsync(user, password))
+        {
+            return _tokenService.GenerateToken(user);
+        }
+
+        return string.Empty;
+    }
+}
