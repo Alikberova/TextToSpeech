fb6b3370feb1766bd5686f30ded9694e83fbc9ef
yevheniia-alikberova <86415242+yevheniia-alikberova@users.noreply.github.com>
2023-12-31 18:06:17 +0100

    Auth (#36)
    * worked on authentication
    
    * interceptors folder

--- BookToAudio.Web/src/app/interceptors/auth.interceptor.ts
+++ BookToAudio.Web/src/app/interceptors/auth.interceptor.ts
@@ -1,5 +1,5 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { Сonstants } from './constants';
+import { Сonstants } from '../constants';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem(Сonstants.AccessToken);
