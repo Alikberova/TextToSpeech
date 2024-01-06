597d17cefcbd44f2098ae20a33c3d6649cce6b9e
yevheniia.alikberova <yevheniia.alikberova@warrcloud.com>
2024-01-06 22:25:20 +0100

    added unit tests

---
+++ BookToAudio.Core/Services/Interfaces/IBtaUserManager.cs
@@ -0,0 +1,14 @@
+﻿using BookToAudio.Core.Entities;
+
+namespace BookToAudio.Core.Services.Interfaces
+{
+    public interface IBtaUserManager
+    {
+        Task<User> CreateAsync(User user);
+        Task DeleteAsync(Guid id);
+        Task<User?> FindByIdAsync(Guid id);
+        Task<User?> FindByNameAsync(string userName);
+        Task<User> UpdateAsync(User user);
+        Task<bool> UserExists(string userName);
+    }
+}
