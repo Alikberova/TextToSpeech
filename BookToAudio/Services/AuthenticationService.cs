81ad8888e14311c59b632334e8f2a2f58cfcf4c8
yevheniia.alikberova <yevheniia.alikberova@warrcloud.com>
2024-01-08 00:16:28 +0100

    fix

--- BookToAudio/Services/AuthenticationService.cs
+++ BookToAudio/Services/AuthenticationService.cs
@@ -1,5 +1,4 @@
 ﻿using BookToAudio.Core.Entities;
-using BookToAudio.Core.Services;
 using BookToAudio.Core.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 
@@ -8,11 +7,11 @@ namespace BookToAudio.Services;
 public class AuthenticationService
 {
     private readonly UserManager<User> _userManager;
-    private readonly BtaUserManager _btaUserManager;
+    private readonly IBtaUserManager _btaUserManager;
     private readonly ITokenService _tokenService;
 
     public AuthenticationService(UserManager<User> userManager,
-        BtaUserManager btaUserManager,
+        IBtaUserManager btaUserManager,
         ITokenService tokenService)
     {
         _userManager = userManager;
