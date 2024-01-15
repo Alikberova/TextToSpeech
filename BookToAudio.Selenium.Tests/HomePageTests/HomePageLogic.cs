b13314ca0662cc9aebf320d3251708ddb6b222e4
ukrBit <ukr.bit.2023@gmail.com>
2024-01-15 13:13:52 +0200

    fix change

--- BookToAudio.Selenium.Tests/HomePageTests/HomePageLogic.cs
+++ BookToAudio.Selenium.Tests/HomePageTests/HomePageLogic.cs
@@ -4,17 +4,18 @@ namespace BookToAudio.Selenium.Tests.HomePageTests;
 
 internal class HomePageLogic
 {
-  private  IWebDriver driver;
+  private readonly IWebDriver _driver;
+
     public HomePageLogic(IWebDriver driver)
     {
-        this.driver = driver;
+        _driver = driver;
     }
 
-   private IWebElement Dropdown => driver.FindElement(By.Id("mat-select-value-1"));
-   private IWebElement PlayVoice => driver.FindElement(By.XPath("//*[@id=\"mat-option-0\"]/mat-icon"));
-   private IWebElement SelectVoice => driver.FindElement(By.Id("mat-option-0"));
-   private IWebElement FileInput => driver.FindElement(By.CssSelector(".file-upload input[type='file']"));
-   private IWebElement SubmitBtn => driver.FindElement(By.XPath("//span[contains(text(), \"Submit\")]"));
+    private IWebElement Dropdown => _driver.FindElement(By.Id("mat-select-value-1"));
+    private IWebElement PlayVoice => _driver.FindElement(By.XPath("//*[@id=\"mat-option-0\"]/mat-icon"));
+    private IWebElement SelectVoice => _driver.FindElement(By.Id("mat-option-0"));
+    private IWebElement FileInput => _driver.FindElement(By.CssSelector(".file-upload input[type='file']"));
+    private IWebElement SubmitBtn => _driver.FindElement(By.XPath("//span[contains(text(), \"Submit\")]"));
 
     public void SelectedVoice()
     {
@@ -23,14 +24,15 @@ internal class HomePageLogic
         Thread.Sleep(4000);
         SelectVoice.Click();
     }
+
     public void SelectedTextFile()
     {
        string textFilePath = @"C:\Users\ukrbi\source\repos\BookToAudio\BookToAudio.Selenium.Tests\TextTest\TextTestAudioBook.txt";
        FileInput.SendKeys(textFilePath);
     }
-     public void ClickSendBtn()
+
+    public void ClickSendBtn()
     {
         SubmitBtn.Click();
     }
-
 }
