fefb6012b65d572a60ce2937a476e219b994cba6
ukrBit <127418989+ukrBit@users.noreply.github.com>
2024-01-15 13:20:13 +0200

    Selenium tests (#68)
    * Create selenim tests
    
    * fix change

---
+++ BookToAudio.Selenium.Tests/HomePageTests/HomePageLogic.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+
+namespace BookToAudio.Selenium.Tests.HomePageTests;
+
+internal class HomePageLogic
+{
+  private readonly IWebDriver _driver;
+
+    public HomePageLogic(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+
+    private IWebElement Dropdown => _driver.FindElement(By.Id("mat-select-value-1"));
+    private IWebElement PlayVoice => _driver.FindElement(By.XPath("//*[@id=\"mat-option-0\"]/mat-icon"));
+    private IWebElement SelectVoice => _driver.FindElement(By.Id("mat-option-0"));
+    private IWebElement FileInput => _driver.FindElement(By.CssSelector(".file-upload input[type='file']"));
+    private IWebElement SubmitBtn => _driver.FindElement(By.XPath("//span[contains(text(), \"Submit\")]"));
+
+    public void SelectedVoice()
+    {
+        Dropdown.Click();
+        PlayVoice.Click();
+        Thread.Sleep(4000);
+        SelectVoice.Click();
+    }
+
+    public void SelectedTextFile()
+    {
+       string textFilePath = @"C:\Users\ukrbi\source\repos\BookToAudio\BookToAudio.Selenium.Tests\TextTest\TextTestAudioBook.txt";
+       FileInput.SendKeys(textFilePath);
+    }
+
+    public void ClickSendBtn()
+    {
+        SubmitBtn.Click();
+    }
+}
---
+++ BookToAudio.Selenium.Tests/RegisterPageTests/RegisterPageTests.cs
@@ -0,0 +1,15 @@
+﻿namespace BookToAudio.Selenium.Tests.RegisterPageTests;
+
+internal class RegisterPageTests: BaseClass
+{
+    [Test]
+    [TestCase("http://localhost:4200/register")]
+    public void  RagisterTests(string url)
+    {
+        driver.Navigate().GoToUrl(url);
+        var registerPage = new RegisterPageLogic(driver);
+        string name = "Anton";
+        string password = "Fynjy1997";
+        registerPage.RegisterUser(name, password);
+    }
+}
