2b7a4e543583488c5300627603bd9ca7dd3bbeeb
yevheniia.alikberova <yevheniia.alikberova@warrcloud.com>
2024-01-16 01:30:06 +0100

    added feedback component & css work

--- BookToAudio.Web/src/app/app-nav/app-nav.component.ts
+++ BookToAudio.Web/src/app/app-nav/app-nav.component.ts
@@ -5,23 +5,24 @@ import { map, shareReplay } from 'rxjs/operators';
 import { AppMaterialModule } from '../app.material/app.material.module';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/auth/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './app-nav.component.html',
   styleUrls: ['./app-nav.component.scss'],
   standalone: true,
-  imports: [ AppMaterialModule, CommonModule ]
+  imports: [ AppMaterialModule, CommonModule, RouterModule ]
 })
 export class AppNavComponent {
 
   constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService,
     private router: Router) {}
 
-  menu = [ 'Feedback' ];
+    menu = [
+      { name: 'Feedback', route: '/feedback' },
+    ];
   
-
   get isLoggedIn() {
     return this.authService.isAuthenticated();
   }
