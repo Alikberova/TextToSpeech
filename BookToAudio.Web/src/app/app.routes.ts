9d5d3ae066c115960b38154a16b89d0e16f86adb
Eugenia <49581014+Alikberova@users.noreply.github.com>
2024-01-19 00:45:46 +0200

    added privacy policy (#72)
    * added privacy policy
    
    * fix
    
    ---------
    
    Co-authored-by: yevheniia.alikberova <yevheniia.alikberova@warrcloud.com>

--- BookToAudio.Web/src/app/app.routes.ts
+++ BookToAudio.Web/src/app/app.routes.ts
@@ -5,14 +5,18 @@ import { RegisterComponent } from './auth-components/register/register.component
 import { QaComponent } from './qa/qa.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
   { path: 'qa', component: QaComponent, pathMatch: 'full' },
   { path: 'contacts', component: ContactsComponent, pathMatch: 'full' },
   { path: "feedback", component: FeedbackFormComponent, pathMatch: 'full'},
-  { path: '**', component: LoginComponent },
+  { path: "privacy-policy", component: PrivacyPolicyComponent, pathMatch: 'full'},
+  { path: "terms-of-use", component: TermsOfUseComponent, pathMatch: 'full'},
+  { path: '**', component: HomeComponent },
 ];
