cafad5f68924a0bd0124a818a813652b1ef1fea3
yevheniia-alikberova <86415242+yevheniia-alikberova@users.noreply.github.com>
2024-01-21 16:46:01 +0100

    accssebility (#77)

--- BookToAudio.Web/src/app/feedback-form/feedback-form.component.ts
+++ BookToAudio.Web/src/app/feedback-form/feedback-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -7,10 +8,13 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-feedback-form',
   standalone: true,
-  imports: [ MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule ],
+  imports: [ MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule ],
   templateUrl: './feedback-form.component.html',
   styleUrl: './feedback-form.component.scss'
 })
 export class FeedbackFormComponent {
+feedback = { name: "lal", email: "qwerty@q.com", message: "Cool service!" };
 
+  submitFeedback() {
+  }
 }
