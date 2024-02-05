50458fd1f43ec6e2403e14fd3a342a0f33bfea58
ukrBit <127418989+ukrBit@users.noreply.github.com>
2024-02-05 13:39:56 +0200

    add validationService (#111)
    * add validationService
    
    * Update feedback-form.component.html
    
    * Update auth-form.component.ts
    
    * Update validation.service.ts
    
    * Update validation.service.ts
    
    * Update feedback-form.component.ts

--- BookToAudio.Web/src/app/feedback-form/feedback-form.component.ts
+++ BookToAudio.Web/src/app/feedback-form/feedback-form.component.ts
@@ -1,27 +1,41 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FeedbackService } from '../services/feedback.service';
+import { ValidationService } from '../services/validation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-feedback-form',
   standalone: true,
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, CommonModule, ReactiveFormsModule],
   templateUrl: './feedback-form.component.html',
   styleUrl: './feedback-form.component.scss'
 })
 export class FeedbackFormComponent {
-feedback = { name: "lal", userEmail: "qwerty@q.com", message: "Cool service!" };
+  feedbackForm: FormGroup;
 
-  constructor(private feedbackService: FeedbackService) { }
+  constructor(private feedbackService: FeedbackService) {
+    this.feedbackForm = new FormGroup({
+      userEmail: new FormControl("",   ValidationService.validationEmail),
+      name: new FormControl("", Validators.required),
+      message: new FormControl("", Validators.required),
+    });
+    this.feedbackForm.setValue({
+      name: "lal", userEmail: "werty@q.com", message: "Cool service!"
+    });
+  }
 
   submitFeedback() {
-    this.feedbackService.feedbackMessageSend(this.feedback).subscribe({
-      next: res => console.log(`status OK 200 \n Ressponce: \p ${res}`),
-      error: err=> console.log(`Error: \p ${err}`)
-    })
+    if (this.feedbackForm.valid) {
+     const feedback = this.feedbackForm.value;
+      this.feedbackService.feedbackMessageSend(feedback).subscribe({
+        next: res => console.log(`status OK 200 \n Ressponce: \p ${res}`),
+        error: err => console.log(`Error: \p ${err}`)
+      });
+    }
   }
 }
---
+++ BookToAudio.Web/src/app/services/validation.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidationService {
+ 
+  static validationEmail(control: AbstractControl): ValidationErrors | null {
+
+    const email = control.value;
+    const emailRegexp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    
+    if (email !== "" && !emailRegexp.test(email)) {
+      return { invalid: true };
+    }
+    return null;
+  }
+}
