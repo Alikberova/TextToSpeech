3b65a06fcef309d43295a0a01315c2fd1155a525
ukrBit <ukr.bit.2023@gmail.com>
2024-02-06 20:12:00 +0200

    fix validation service

--- BookToAudio.Web/src/app/services/validation.service.ts
+++ BookToAudio.Web/src/app/services/validation.service.ts
@@ -21,18 +21,18 @@ export class ValidationService {
     const password = control.value;
     let errMessage = "";    
 
-      if (/(\s)/.test(password)) {
-        errMessage += "The password must not contain spaces.\n"
-      }
-      if (!/.{6}/.test(password)) {
-        errMessage += "The password must be at least 6 characters long.\n"
-      }
-      if (!/([A-Z])/.test(password)) {
-        errMessage += "The password must contain at least one capital letter.\n"
-      }
-      if (!/([\W_])/.test(password)) {
-        errMessage += "The password must contain at least one special character.\n"
-      }
+    if (/(\s)/.test(password)) {
+      errMessage += "The password must not contain spaces.\n"
+    }
+    if (!/.{6}/.test(password)) {
+      errMessage += "The password must be at least 6 characters long.\n"
+    }
+    if (!/([A-Z])/.test(password)) {
+      errMessage += "The password must contain at least one capital letter.\n"
+    }
+    if (!/([\W_])/.test(password)) {
+      errMessage += "The password must contain at least one special character.\n"
+    }
     
     if (errMessage !== "") {
       return { invalid: { message: errMessage } }
