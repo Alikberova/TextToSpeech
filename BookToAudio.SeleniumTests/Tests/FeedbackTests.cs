71615e275dffb115a0a9d8530a57dfe0d29a7ba7
yevheniia-alikberova <86415242+yevheniia-alikberova@users.noreply.github.com>
2024-03-21 00:23:06 +0100

    change selenium to xunit (#172)
    * change selenium to xunit
    
    * update bogus
    
    * Revert "change selenium to xunit"
    
    This reverts commit 850109518af83a90dd3e04b99ee1a82b341dc6af.
    
    * Revert "update bogus"
    
    This reverts commit c73f027a98792834fbca3749ea35687fa1454cf9.
    
    * fix

---
+++ BookToAudio.SeleniumTests/Tests/FeedbackTests.cs
@@ -0,0 +1,27 @@
+﻿using BookToAudio.Core.Dto;
+using BookToAudio.SeleniumTests.Pages;
+
+namespace BookToAudio.SeleniumTests.Tests;
+
+public sealed class FeedbackTests : TestBase
+{
+    private const string SuccessMessage = "Your feedback was sent. Thanks!";
+
+    [Fact]
+    public void SubmitFeedback()
+    {
+        var request = new EmailRequest()
+        {
+            Name = "Jane",
+            UserEmail = "jane@qwerty.com",
+            Message = "Lorem ipsum"
+        };
+
+        var page = new FeedbackPage(Driver, Wait);
+        page.ClickMenu();
+        page.FillForm(request);
+        page.Submit();
+
+        Assert.Contains(SuccessMessage, page.GetSnackBarText());
+    }
+}
