ec7ab64942b17745d3e00642f9bc4ba3c36f8dc7
yevheniia.alikberova <yevheniia.alikberova@warrcloud.com>
2024-04-17 20:55:47 +0200

    Feedback form: Show red color only if Send button pressed

--- BookToAudio.Web/src/app/services/validation.service.ts
+++ BookToAudio.Web/src/app/services/validation.service.ts
@@ -5,18 +5,6 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
   providedIn: 'root'
 })
 export class ValidationService {
- 
-  static validationEmail(control: AbstractControl): ValidationErrors | null {
-
-    const email = control.value;
-    const emailRegexp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    
-    if (email !== "" && !emailRegexp.test(email)) {
-      return { invalid: true };
-    }
-    return null;
-  }
-
   static matchRequiredPassword(control: AbstractControl): ValidationErrors | null {
     const password = control.value;
     let errMessage = "";    
