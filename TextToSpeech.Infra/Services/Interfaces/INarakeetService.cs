c00f5587cdbf3566d69464bd789f8a827f02714c
yevheniia-alikberova <86415242+yevheniia-alikberova@users.noreply.github.com>
2024-04-28 22:36:04 +0200

    added voices endpoint (#211)
    * added voices endpoint
    
    * fix to _redisConnection

---
+++ TextToSpeech.Infra/Services/Interfaces/INarakeetService.cs
@@ -0,0 +1,9 @@
+﻿using TextToSpeech.Core.Dto.Narakeet;
+using TextToSpeech.Core.Services.Interfaces;
+
+namespace TextToSpeech.Infra.Services.Interfaces;
+
+public interface INarakeetService : ITtsService
+{
+    Task<List<VoiceResponse>?> GetAvailableVoices();
+}
---
+++ TextToSpeech.Infra/Services/RedisCacheProvider.cs
@@ -0,0 +1,51 @@
+﻿using StackExchange.Redis;
+using System.Text.Json;
+using TextToSpeech.Infra.Services.Interfaces;
+
+namespace TextToSpeech.Infra.Services;
+
+public class RedisCacheProvider : IRedisCacheProvider
+{
+    private readonly ConnectionMultiplexer? _redisConnection;
+
+    public RedisCacheProvider(string connectionString)
+    {
+        try
+        {
+            _redisConnection = ConnectionMultiplexer.Connect(connectionString);
+        }
+        catch (RedisConnectionException)
+        {
+        }
+    }
+
+    public async Task<T?> GetCachedData<T>(string key)
+    {
+        if (_redisConnection is null)
+        {
+            return default;
+        }
+
+        var db = _redisConnection.GetDatabase();
+        var cachedData = await db.StringGetAsync(key);
+
+        if (!cachedData.IsNullOrEmpty)
+        {
+            return JsonSerializer.Deserialize<T>(cachedData!);
+        }
+
+        return default;
+    }
+
+    public async Task SetCachedData<T>(string key, T data, TimeSpan expiry)
+    {
+        if (_redisConnection is null)
+        {
+            return;
+
+        }
+        var db = _redisConnection.GetDatabase();
+        var serializedData = JsonSerializer.Serialize(data);
+        await db.StringSetAsync(key, serializedData, expiry);
+    }
+}
