40e57af553fd5733ba80bcfe94582a197dafb8e1
yevheniia.alikberova <yevheniia.alikberova@warrcloud.com>
2024-04-29 20:38:37 +0200

    fix

--- TextToSpeech.Infra/AppDbContext.cs
+++ TextToSpeech.Infra/AppDbContext.cs
@@ -4,13 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TextToSpeech.Infra;
 
-public class AppDbContext : IdentityDbContext<User>
+public sealed class AppDbContext : IdentityDbContext<User>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
-
-    public AppDbContext() { }
 
     public DbSet<AudioFile> AudioFiles { get; set; }
 }
