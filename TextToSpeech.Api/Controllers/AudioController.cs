976e9252a042b7cc82483e003c395314c3196c37
yevheniia.alikberova <yevheniia.alikberova@warrcloud.com>
2024-06-08 22:57:04 +0200

    fix

--- TextToSpeech.Api/Controllers/AudioController.cs
+++ TextToSpeech.Api/Controllers/AudioController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Net.Http.Headers;
 using TextToSpeech.Core.Interfaces;
+using TextToSpeech.Core.Interfaces.Repositories;
 
 namespace TextToSpeech.Api.Controllers;
 
@@ -9,49 +10,44 @@ namespace TextToSpeech.Api.Controllers;
 public sealed class AudioController : ControllerBase
 {
     private readonly IPathService _pathService;
+    private readonly IAudioFileRepository _audioFileRepository;
 
-    public AudioController(IPathService pathService)
+    public AudioController(IPathService pathService, IAudioFileRepository audioFileRepository)
     {
         _pathService = pathService;
+        _audioFileRepository = audioFileRepository;
     }
 
     [HttpGet("downloadmp3/{fileId}/{fileName}")]
-    public IActionResult DownloadMp3(string fileId, string fileName)
+    public async Task<IActionResult> DownloadMp3(string fileId, string fileName)
     {
         string filePath = _pathService.GetFileStorageFilePath($"{fileId}.mp3");
 
         if (!System.IO.File.Exists(filePath))
         {
+            var dbAudioFile = await _audioFileRepository.GetAudioFileAsync(Guid.Parse(fileId));
+
+            if (dbAudioFile != null)
+            {
+                SetHeader(fileName);
+                return File(dbAudioFile.Data, "audio/mpeg");
+            }
+
             return NotFound("File not found.");
         }
 
+        SetHeader(fileName);
+
+        return File(System.IO.File.ReadAllBytes(filePath), "audio/mpeg");
+    }
+
+    private void SetHeader(string fileName)
+    {
         var contentDisposition = new ContentDispositionHeaderValue("attachment")
         {
             FileName = fileName
         };
 
         Response.Headers[HeaderNames.ContentDisposition] = contentDisposition.ToString();
-
-        return File(System.IO.File.ReadAllBytes(filePath), "audio/mpeg");
-    }
-
-    [HttpGet]
-    [Route("streammp3/{fileId}")]
-    public async Task<IActionResult> StreamMp3(string fileId)
-    {
-        string filePath = _pathService.GetFileStorageFilePath($"{fileId}.mp3");
-
-        if (!System.IO.File.Exists(filePath))
-        {
-            return NotFound("File not found.");
-        }
-
-        var memoryStream = new MemoryStream();
-        using (var stream = new FileStream(filePath, FileMode.Open))
-        {
-            await stream.CopyToAsync(memoryStream);
-        }
-        memoryStream.Position = 0;
-        return File(memoryStream, "audio/mpeg", Path.GetFileName(filePath), true);
     }
 }
