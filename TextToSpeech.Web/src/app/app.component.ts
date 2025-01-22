b08264f1e526f149de1a9090d951cb8ac61cb811
yevheniia-alikberova <86415242+yevheniia-alikberova@users.noreply.github.com>
2025-01-22 23:07:31 +0100

    added mobile menu (#245)
    * added mobile menu
    
    * css for mobiles
    
    * scrollPositionRestoration
    
    * ScrollButtonComponent, removed fixed menu
    
    * inline-block
    
    * test viewport-fit=cover for ios
    
    * mat-toolbar test

--- TextToSpeech.Web/src/app/app.component.ts
+++ TextToSpeech.Web/src/app/app.component.ts
@@ -5,13 +5,14 @@ import { RouterOutlet } from "@angular/router";
 import { AppNavComponent } from "./components/app-nav/app-nav.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { HomeComponent } from "./components/home/home.component";
+import { ScrollButtonComponent } from "./components/scroll-button/scroll-button.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, FormsModule, RouterOutlet, AppNavComponent, HomeComponent, FooterComponent]
+  imports: [CommonModule, FormsModule, RouterOutlet, AppNavComponent, HomeComponent, FooterComponent, ScrollButtonComponent]
 })
 export class AppComponent {
 }
