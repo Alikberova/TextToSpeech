105f9e00a40bd7b4074efec1c3d5c470e8586321
Yevheniia Alikberova <e.alikberova@outlook.com>
2025-12-26 13:05:37 +0100

    minor fixes

--- TextToSpeech.Api/Controllers/AudioController.cs
+++ TextToSpeech.Api/Controllers/AudioController.cs
@@ -27,7 +27,7 @@ public sealed class AudioController : ControllerBase
             return BadRequest("Invalid file ID.");
         }
 
-        var dbAudioFile = await _audioFileRepository.GetAudioFileAsync(Guid.Parse(fileId));
+        var dbAudioFile = await _audioFileRepository.GetAudioFileAsync(parsedFileId);
 
         if (dbAudioFile is null)
         {
